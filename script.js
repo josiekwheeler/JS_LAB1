44dce92b3932ba428916f1391a5f646190bc11a1
Josie <josiekwheeler@gmail.com>
2019-04-29 20:31:48 -0400

    lab1part1

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 "use strict"
 
 let user = undefined;
-let userHealth = 40;
-let grantHealth = 10;
+let userLife = 40;
+let grantLife = 10;
 let userWins = 0;
 let grantWins = 0;
-let userDamage = 0;
-let grantDamage = 0;
+let userLost = 0;
+let grantLost = 0;
 let game = prompt("Do you want to play a game with the Alimighty Grant?", "yes or no");
 
 
 if (game === "yes") {
     user = prompt("Choose your user name", "Josie Wheeler");
     while (grantWins < 1 && userWins < 3) {
-        if (userHealth > 0 && grantHealth > 0) {
-            userDamage = Math.floor(Math.random() * 2) + 1;
-            grantDamage = Math.floor(Math.random() * 2) + 1;
-            userHealth = userHealth - userDamage;
-            grantHealth = grantHealth - grantDamage;
-            console.log(`${user} has ${userHealth} remaining`);
-            console.log(`Grant has ${grantHealth} remaining`);
-        } else if (grantHealth <= 0) {
+        if (userLife > 0 && grantLife > 0) {
+            userLost = Math.floor(Math.random() * 2) + 1;
+            grantLost = Math.floor(Math.random() * 2) + 1;
+            userLife = userLife - userLost;
+            grantLife = grantLife - grantLost;
+            console.log(`${user} has ${userLife} remaining`);
+            console.log(`Grant has ${grantLife} remaining`);
+        } else if (grantLife <= 0) {
             userWins++;
-            grantHealth = 10;
+            grantLife = 10;
             console.log(`${user} wins ${userWins} rounds`);
-        } else if (userHealth <= 0) {
+        } else if (userLife <= 0) {
             console.log(`Grant Wins!`);
             break;
         }
